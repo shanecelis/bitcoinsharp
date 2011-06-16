bfdbeed8461aec26255f294c28030c374e386eec
nathan.baulch <nathan.baulch@121d5508-dec5-6b30-03f7-cbaacbc583fd>
2011-06-16 04:52:38 +0000

    Update to BitCoinJ revision 95: Add a SeedPeers class that contains a pre-compiled list of IP addresses taking part in the Bitcoin network for a long period of time, for use if IRC and DNS are unavailable. Based on a patch by Micheal Swiggs.

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.91.9")]
-[assembly: AssemblyFileVersion("0.1.91.9")]
+[assembly: AssemblyVersion("0.1.95.11")]
+[assembly: AssemblyFileVersion("0.1.95.11")]
 [assembly: InternalsVisibleTo("BitCoinSharp.Test")]
