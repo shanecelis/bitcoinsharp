fca82c97e00def2cca3f81d3f581564814284062
nathan.baulch <nathan.baulch@121d5508-dec5-6b30-03f7-cbaacbc583fd>
2011-06-28 03:53:24 +0000

    Increment version to 0.2

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.110.21")]
-[assembly: AssemblyFileVersion("0.1.110.21")]
+[assembly: AssemblyVersion("0.2.111.22")]
+[assembly: AssemblyFileVersion("0.2.111.22")]
 [assembly: InternalsVisibleTo("BitCoinSharp.Test")]
